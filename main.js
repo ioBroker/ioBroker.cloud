f090bba94d5e00c718dafa756632160258e386b4
Ingo Fischer <github@fischer-ka.de>
2019-01-08 21:18:40 +0100

    change check for compact mode

--- main.js
+++ main.js
@@ -914,7 +914,7 @@ function main() {
 }
 
 // If started as allInOne/compact mode => return function to create instance
-if (typeof module !== undefined && module.parent) {
+if (module.parent) {
     module.exports = startAdapter;
 } else {
     // or start the instance directly
